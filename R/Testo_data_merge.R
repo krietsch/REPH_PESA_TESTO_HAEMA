237c4e60c47c0d4fbf1f68b11289eb2653c91d60
krietsch <jkrietsch@orn.mpg.de>
2023-08-25 15:00:05 +0200

    initial push with first look at data

---
+++ R/Testo_data_merge.R
@@ -0,0 +1,111 @@
+#========================================================================================================================
+# Merge data from REPH, PESA and SESA
+#========================================================================================================================
+
+sapply( c('data.table', 'sdb', 'magrittr', 'ggplot2', 'DBI'),
+        require, character.only = TRUE)
+
+#-------------------------------------------------------------------------------------------------------------------------
+# REPH data
+#-------------------------------------------------------------------------------------------------------------------------
+
+con = dbcon('jkrietsch', db = 'REPHatBARROW')  
+
+# data
+dc = dbq(con, 'select * FROM CAPTURES')
+dt = dbq(con, 'select * FROM TESTO')
+ds = dbq(con, 'select * FROM SEX')
+dbDisconnect(con)
+
+# merge with lab sex
+dc = merge(dc, ds[, .(ID = as.integer(ID), sex)], by = 'ID', all.x = TRUE)
+
+# add date
+dt[, date_ := as.Date(date_)]
+dc[, date_ := as.Date(caught_time)]
+
+dc = merge(dc[, !c('GnRH'), with = FALSE], dt[, .(ID, date_, GnRH, volume, T)], by = c('ID', 'date_'), all.x = TRUE)
+
+dR = dc[!is.na(T), .(species = 'REPH', ID, date_, caught_time, bled_time, sex_observed = sex_observed, sex_genetic = sex, 
+                     testo = T, volume, GnRH)]
+
+#-------------------------------------------------------------------------------------------------------------------------
+# PESA data
+#-------------------------------------------------------------------------------------------------------------------------
+
+con = dbcon('jkrietsch', db = 'PESAatBARROW')  
+
+# data
+dc = dbq(con, 'select * FROM CAPTURES')
+dt = dbq(con, 'select * FROM TESTO')
+ds = dbq(con, 'select * FROM SEX')
+dbDisconnect(con)
+
+# merge with lab sex
+dc = merge(dc, ds[, .(ID = as.integer(ID), sex)], by = 'ID', all.x = TRUE)
+dc[sex == 1, sex_genetic := 'M']
+dc[sex == 2, sex_genetic := 'F']
+
+setnames(dc, c('start_capture_date_time'), c('caught_time'))
+dc[, sex_observed := as.character(sex_observed)]
+dc[sex_observed == 1, sex_observed := 'M']
+dc[sex_observed == 2, sex_observed := 'F']
+
+# add date
+dt[, date_ := as.Date(date_)]
+dc[, date_ := as.Date(caught_time)]
+
+dc = merge(dc, dt[, .(ID, date_, GnRH, volume, T)], by = c('ID', 'date_'), all.x = TRUE)
+
+dP = dc[!is.na(T), .(species = 'PESA', ID, date_, caught_time = caught_date_time, bled_time = bled_date_time, 
+                     sex_observed = sex_observed, sex_genetic = sex_genetic, testo = T, volume, GnRH)]
+
+#-------------------------------------------------------------------------------------------------------------------------
+# SESA data
+#-------------------------------------------------------------------------------------------------------------------------
+
+con = dbcon('jkrietsch', db = 'EXTRA_AVESatBARROW')  
+
+# data
+dt = dbq(con, 'select * FROM 2004_HORMONES')
+dbDisconnect(con)
+
+con = dbcon('jkrietsch', db = 'AVESatBARROW')  
+
+# data
+dc = dbq(con, 'select * FROM CAPTURES')
+ds = dbq(con, 'select * FROM SEX')
+dbDisconnect(con)
+
+# add date
+dt[, date_ := as.Date(date_time)]
+dc[, date_ := as.Date(caught_date_time)]
+
+# exclude NA
+dc = dc[!is.na(ID)]
+ds[, ID := as.integer(ID)]
+ds = ds[!is.na(ID)]
+
+# merge with lab sex
+dc = merge(dc, ds[, .(ID = as.integer(ID), sex)], by = 'ID', all.x = TRUE)
+dc[sex == 1, sex_genetic := 'M']
+dc[sex == 2, sex_genetic := 'F']
+
+# merge with testo
+dc = merge(dc, dt[, .(ID, date_, GnRH = NA, volume = NA, testo)], by = c('ID', 'date_'), all.x = TRUE)
+
+dS = dc[!is.na(testo), .(species = 'SESA', ID, date_, caught_time = caught_date_time, bled_time = bled_date_time, 
+                         sex_observed = sex_observed, sex_genetic = sex_genetic, testo, volume = NA, GnRH = NA)]
+
+# sex missing for most birds?
+dS[, .N, sex_observed]
+dS[, .N, sex_genetic]
+
+#-------------------------------------------------------------------------------------------------------------------------
+# merge species
+#-------------------------------------------------------------------------------------------------------------------------
+
+d = rbindlist(list(dR, dP, dS))
+d[, year_ := year(date_)]
+
+# saveRDS(d, './DATA/REPH_PESA_SESA_testosterone.RDS')
